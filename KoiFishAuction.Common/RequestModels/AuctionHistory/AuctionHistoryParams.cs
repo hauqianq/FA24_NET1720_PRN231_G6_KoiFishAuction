67dce57abeb09aa687786da2af375ac9035116d4
Nguyen Dinh Hoang Huy <0788909221ndhh@gmail.com>
2024-10-25 12:54:34 +0700

    add auction history service

---
+++ KoiFishAuction.Common/RequestModels/AuctionHistory/AuctionHistoryParams.cs
@@ -0,0 +1,24 @@
+﻿namespace KoiFishAuction.Common.RequestModels.AuctionHistory;
+
+public sealed class AuctionHistoryParams : PaginationRequest
+{
+    public string? FeedbackStatus { get; set; }
+    public string? DeliveryStatus { get; set; }
+    public string? PaymentStatus { get; set; }
+    public string? OrderBy { get; set; }
+};
+
+public abstract class PaginationRequest
+{
+    private const int MaxPageSize = 25;
+
+    public int PageNumber { get; set; } = 1;
+
+    private int _pageSize = 10;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+}
--- KoiFishAuction.Data/Repository/AuctionHistoryRepository.cs
+++ KoiFishAuction.Data/Repository/AuctionHistoryRepository.cs
@@ -1,9 +1,36 @@
-﻿using KoiFishAuction.Data.Base;
+﻿using System.Linq.Expressions;
+using KoiFishAuction.Data.Base;
 using KoiFishAuction.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace KoiFishAuction.Data.Repository
 {
     public class AuctionHistoryRepository : GenericRepository<AuctionHistory>
     {
+        public async Task<List<AuctionHistory>> GetAllAuctionHistory(
+            Expression<Func<AuctionHistory, bool>>? predicate,
+            Func<IQueryable<AuctionHistory>, IOrderedQueryable<AuctionHistory>>? orderBy,
+            Func<IQueryable<AuctionHistory>, IIncludableQueryable<AuctionHistory, object?>>? includeEntity,
+            bool tracking = false)
+        {
+            IQueryable<AuctionHistory> query = tracking ? _context.AuctionHistories.AsQueryable() : _context.AuctionHistories.AsNoTracking();
+            if (includeEntity != null)
+            {
+                query = includeEntity(query);
+            }
+            
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+
+            return await query.ToListAsync();
+        }
+        
     }
 }
